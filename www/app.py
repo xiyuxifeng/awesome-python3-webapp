95a5934b0afcfa1b2132cf453012d62157f47d63
WH <xiyuxifeng@163.com>
2017-12-04 23:04:53 +0800

    add app.py

---
+++ www/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+###
+# File: /app.py
+# Project: www
+# Created Date: Monday, December 4th 2017, 11:04:27 pm
+# Author: Wang Hui
+# -----
+# Last Modified: Mon Dec 04 2017
+# Modified By: Wang Hui
+# -----
+# Copyright (c) 2017 WH
+# ------------------------------------
+# Life is short, you need Python
+###
