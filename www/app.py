b7447c44a84fec9bc2cd43e8d5998f5c0ab05476
WH <xiyuxifeng@163.com>
2017-12-05 23:35:42 +0800

    day1

--- www/app.py
+++ www/app.py
@@ -6,10 +6,35 @@
 # Created Date: Monday, December 4th 2017, 11:04:27 pm
 # Author: Wang Hui
 # -----
-# Last Modified: Mon Dec 04 2017
+# Last Modified: Tue Dec 05 2017
 # Modified By: Wang Hui
 # -----
 # Copyright (c) 2017 WH
 # ------------------------------------
 # Life is short, you need Python
 ###
+
+import logging
+logging.basicConfig(level=logging.INFO)
+import asyncio, os, json, time
+from datetime import datetime
+from aiohttp import web
+
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000....')
+    return srv
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
+
---
+++ www/orm.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+###
+# File: /orm.py
+# Project: www
+# Created Date: Tuesday, December 5th 2017, 10:21:11 pm
+# Author: Wang Hui
+# -----
+# Last Modified: Tue Dec 05 2017
+# Modified By: Wang Hui
+# -----
+# Copyright (c) 2017 WH
+# ------------------------------------
+# Life is short, you need Python
+###
+
+import asyncio
+import aiomysql
+import logging
+
+'创建连接池'
+
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
+
+
+'Select'
+@asyncio.coroutine
+def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    with (yield from __pool) as conn:
+        cur = yield from conn.cursor(aiomysql.DictCursor)
+        yield from cur.execute(sql.replace('?', '%s'), args or ())
+        if size:
+            rs = yield from cur.fetchmany(size)
+        else:
+            rs = yield from cur.fetchall()
+        yield from cur.close()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+
+'Insert, Update, Delete'
+@asyncio.coroutine
+def execute(sql, args):
+    log(sql)
+    with (yield from __pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?', '%s'), args)
+            affected = cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return affected
+
