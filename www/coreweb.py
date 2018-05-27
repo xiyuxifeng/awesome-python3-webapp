e31ff7ba0c20d94ef189f78bc2fd7c29a0e78034
WH <xiyuxifeng@163.com>
2018-05-27 21:14:34 +0800

    day-12

--- www/coreweb.py
+++ www/coreweb.py
@@ -128,7 +128,7 @@ class RequestHandler(object):
                 qs = request.query_string
                 if qs:
                     kw = dict()
-                    for k, v in parse.parse_qs(qs, True).items:
+                    for k, v in parse.parse_qs(qs, True).items():
                         kw[k] = v[0]
 
         if kw is None:
--- www/handlers.py
+++ www/handlers.py
@@ -23,7 +23,7 @@ import markdown2
 from aiohttp import web
 
 from coreweb import get, post
-from apis import APIError, APIValueError, APIResourceNotFoundError, APIPermissionError
+from apis import APIError, APIValueError, APIResourceNotFoundError, APIPermissionError, Page
 
 from models import User, Comment, Blog, next_id
 from config import configs
@@ -131,6 +131,15 @@ def signin():
     }
 
 
+@get('/signout')
+def signout(request):
+    referer = request.headers.get('Referer')
+    r = web.HTTPFound(referer or '/')
+    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
+    logging.info('user signed out.')
+    return r
+
+
 @post('/api/authenticate')
 async def authenticate(*, email, passwd):
     if not email:
@@ -162,15 +171,6 @@ async def authenticate(*, email, passwd):
     return r
 
 
-@get('/signout')
-def signout(request):
-    referer = request.headers.get('Referer')
-    r = web.HTTPFound(referer or '/')
-    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
-    logging.info('user signed out.')
-    return r
-
-
 @get('/blog/{id}')
 async def get_blog(id):
     blog = await Blog.find(id)
@@ -188,12 +188,31 @@ async def get_blog(id):
     }
 
 
+@get('/manage/blogs')
+def manage_blogs(request, *, page='1'):
+    return {
+        '__template__': 'manage_blogs.html',
+        'page_index': get_page_index(page),
+        '__user__': request.__user__
+    }
+
+
 @get('/manage/blogs/create')
 def manage_create_blog(request):
     return {
         '__template__': 'manage_blog_edit.html',
         'id': '',
-        'action': '/api/blogs',
+        'action': '/api/blogs/add',
+        '__user__': request.__user__
+    }
+
+
+@get('/manage/blogs/edit')
+def manage_edit_blog(request, *, id):
+    return {
+        '__template__': 'manage_blog_edit.html',
+        'id': id,
+        'action': '/api/blogs/update/%s' % id,
         '__user__': request.__user__
     }
 
@@ -238,13 +257,7 @@ async def api_get_user(*, id):
     return user
 
 
-@get('/api/blogs/{id}')
-async def api_get_blog(*, id):
-    blog = await Blog.find(id)
-    return blog
-
-
-@post('/api/blogs')
+@post('/api/blogs/add')
 async def api_create_blog(request, *, name, summary, content):
     check_admin(request)
     if not name or not name.strip():
@@ -256,3 +269,53 @@ async def api_create_blog(request, *, name, summary, content):
     blog = Blog(id=next_id(), user_id=request.__user__.id, user_name=request.__user__.name, user_image=request.__user__.image, name=name.strip(), summary=summary.strip(), content=content.strip())
     await blog.save()
     return blog
+
+
+@post('/api/blogs/update/{id}')
+async def api_update_blog(id, request, *, name, summary, content):
+    check_admin(request)
+    blog = await Blog.find(id)
+    if not blog:
+        raise APIValueError('id', 'cannot find this blog, may be already deleted')
+    else:
+        if not name or not name.strip():
+            raise APIValueError('name', 'name cannot be empty.')
+        if not summary or not summary.strip():
+            raise APIValueError('summary', 'summary cannot be empty.')
+        if not content or not content.strip():
+            raise APIValueError('content', 'content cannot be empty.')
+        blog.name = name.strip()
+        blog.summary = summary.strip()
+        blog.content = content.strip()
+        await blog.update()
+        return blog
+
+
+@post('/api/blogs/delete/{id}')
+async def api_delete_blog(request, *, id):
+    check_admin(request)
+    blog = await Blog.find(id)
+    if not blog:
+        raise APIValueError('id', 'cannot find this blog, may be already deleted')
+    else:
+        await blog.remove()
+        return dict(id=id)
+
+
+
+
+@get('/api/blogs/{id}')
+async def api_get_blog(*, id):
+    blog = await Blog.find(id)
+    return blog
+
+
+@get('/api/blogs')
+async def api_blogs(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Blog.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, blogs=())
+    blogs = await Blog.findAll(orderBy='create_at desc', limit=(p.offset, p.limit))
+    return dict(page=p, blogs=blogs)
